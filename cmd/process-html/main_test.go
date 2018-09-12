17178ce80b3de88c18535f84be767ad70a69ac4a
José Moreno <jomoespe@gmail.com>
2018-09-12 18:43:44 +0200

    Add HTML tag attribute test

--- cmd/process-html/main_test.go
+++ cmd/process-html/main_test.go
@@ -10,12 +10,13 @@ import (
 
 func TestProcessHTMLTasgs(t *testing.T) {
 	tt := [...]struct {
-		filename string
-		tag      string
-		total    int
+		filename  string
+		validator func(n *html.Node) bool
+		total     int
 	}{
-		{"../../testdata/process-html/example-1.html", "section", 3},
-		{"../../testdata/process-html/example-1.html", "p", 2},
+		{"../../testdata/process-html/example-1.html", tag("section"), 3},
+		{"../../testdata/process-html/example-1.html", tag("p"), 2},
+		{"../../testdata/process-html/example-1.html", attribute("data-src"), 3},
 	}
 
 	for _, test := range tt {
@@ -24,22 +25,18 @@ func TestProcessHTMLTasgs(t *testing.T) {
 		if err != nil {
 			t.Fatalf("cannot parse testfile %s", test.filename)
 		}
-		// TODO here to process the HTML looking for tag type
-		//		t.Errorf("Type: %v", doc.Type)
-
 		tags := 0
-
-		var f func(*html.Node)
-		f = func(n *html.Node) {
-			if n.Type == html.ElementNode && n.Data == test.tag {
+		var f func(func(*html.Node) bool, *html.Node)
+		f = func(predicate func(*html.Node) bool, n *html.Node) {
+			if predicate(n) {
 				tags++
 			} else {
 				for c := n.FirstChild; c != nil; c = c.NextSibling {
-					f(c)
+					f(predicate, c)
 				}
 			}
 		}
-		f(doc)
+		f(test.validator, doc)
 
 		if test.total != tags {
 			t.Fatalf("expected: %d, got: %v", test.total, tags)
@@ -47,4 +44,26 @@ func TestProcessHTMLTasgs(t *testing.T) {
 	}
 }
 
-// TODO Add another test to look for 'data-src' attribute
+// tag returns a predicate function that cheks if a node is for a tag type
+func tag(tagType string) func(*html.Node) bool {
+	return func(n *html.Node) bool {
+		return n.Type == html.ElementNode && n.Data == tagType
+	}
+}
+
+// tag returns a predicate function that cheks if a node contains a attribute
+func attribute(attr string) func(*html.Node) bool {
+	containsAttr := func(attr string, attrs []html.Attribute) bool {
+		for _, a := range attrs {
+			if a.Key == attr {
+				return true
+			}
+		}
+		return false
+	}
+	return func(n *html.Node) bool {
+		return n.Type == html.ElementNode && containsAttr(attr, n.Attr)
+	}
+}
+
+// TODO Add DOM content replacement test
