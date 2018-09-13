e0c0ee2111122b732d90fc40f83c0d61bed9fc5d
José Moreno <jomoespe@gmail.com>
2018-09-13 18:09:19 +0200

    Add HTML visitor test

--- cmd/process-html/main_test.go
+++ cmd/process-html/main_test.go
@@ -1,30 +1,35 @@
 package main_test
 
 import (
+	"fmt"
 	"os"
+	"sync"
 
 	"golang.org/x/net/html"
 
 	"testing"
 )
 
+type predicate func(*html.Node) bool
+type visitor func(*sync.WaitGroup, *html.Node)
+
 func TestProcessHTMLTasgs(t *testing.T) {
 	tt := [...]struct {
 		filename  string
-		validator func(n *html.Node) bool
+		predicate predicate
 		total     int
 	}{
 		{"../../testdata/process-html/example-1.html", tag("section"), 3},
 		{"../../testdata/process-html/example-1.html", tag("p"), 2},
 		{"../../testdata/process-html/example-1.html", attribute("data-src"), 3},
 	}
-
 	for _, test := range tt {
 		b, _ := os.Open(test.filename)
 		doc, err := html.Parse(b)
 		if err != nil {
 			t.Fatalf("cannot parse testfile %s", test.filename)
 		}
+
 		tags := 0
 		var f func(func(*html.Node) bool, *html.Node)
 		f = func(predicate func(*html.Node) bool, n *html.Node) {
@@ -36,7 +41,7 @@ func TestProcessHTMLTasgs(t *testing.T) {
 				}
 			}
 		}
-		f(test.validator, doc)
+		f(test.predicate, doc)
 
 		if test.total != tags {
 			t.Fatalf("expected: %d, got: %v", test.total, tags)
@@ -44,16 +49,58 @@ func TestProcessHTMLTasgs(t *testing.T) {
 	}
 }
 
+func ExampleVisitNodes() {
+	filename := "../../testdata/process-html/example-1.html"
+	isSection := tag("section")
+	printNode := printNodeData()
+
+	b, _ := os.Open(filename)
+	doc, err := html.Parse(b)
+	if err != nil {
+		fmt.Printf("cannot parse testfile %s", filename)
+	}
+
+	var f func(*sync.WaitGroup, *html.Node, predicate, visitor)
+	f = func(wg *sync.WaitGroup, node *html.Node, p predicate, v visitor) {
+		defer wg.Done()
+		if p(node) {
+			wg.Add(1)
+			go v(wg, node)
+		} else {
+			for child := node.FirstChild; child != nil; child = child.NextSibling {
+				wg.Add(1)
+				f(wg, child, p, v)
+			}
+		}
+	}
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go f(&wg, doc, isSection, printNode)
+	wg.Wait()
+	// Output:
+	// section
+	// section
+	// section
+}
+
+// printNodeData returns a node visitor that print the node.Data
+func printNodeData() visitor {
+	return func(wg *sync.WaitGroup, n *html.Node) {
+		defer wg.Done()
+		fmt.Println(n.Data)
+	}
+}
+
 // tag returns a predicate function that cheks if a node is for a tag type
-func tag(tagType string) func(*html.Node) bool {
+func tag(tagType string) predicate {
 	return func(n *html.Node) bool {
 		return n.Type == html.ElementNode && n.Data == tagType
 	}
 }
 
 // tag returns a predicate function that cheks if a node contains a attribute
-func attribute(attr string) func(*html.Node) bool {
-	containsAttr := func(attr string, attrs []html.Attribute) bool {
+func attribute(attr string) predicate {
+	contains := func(attr string, attrs []html.Attribute) bool {
 		for _, a := range attrs {
 			if a.Key == attr {
 				return true
@@ -62,7 +109,7 @@ func attribute(attr string) func(*html.Node) bool {
 		return false
 	}
 	return func(n *html.Node) bool {
-		return n.Type == html.ElementNode && containsAttr(attr, n.Attr)
+		return n.Type == html.ElementNode && contains(attr, n.Attr)
 	}
 }
 
