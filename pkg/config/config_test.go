5b8dec60aedfaa7108102af734e9f3198854683e
José Moreno <jomoespe@gmail.com>
2018-09-17 18:14:04 +0200

    Add LICENSE and source files headers

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -1,3 +1,7 @@
+// Copyright 2010 Jomoespe. All rights reserved.
+// Use of this source code is governed by a WTFPL-style
+// license that can be found in the LICENSE file.
+
 package config_test
 
 import (
